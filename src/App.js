d382a56c48cdebbae560c6b128a9542981894f72
mathieumarcelino <marcelinomathieu91@gmail.com>
2022-01-06 17:53:25 +0100

    update config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Home />}>
         </Route>
